0d8dc747d460162e716d2de48d5281454482e195
chaitanya0411 <chaitanyakulkarni0411@gmail.com>
2016-12-13 12:46:53 -0500

    Added Conway's Game Of Life !

---
+++ GameOfLife.java
@@ -0,0 +1,123 @@
+package games;
+
+
+//https://en.wikipedia.org/wiki/Conway's_Game_of_Life
+
+public class GameOfLife
+{    
+    public void gameOfLife(int[][] board)
+    {
+        if(board.length > 0)
+        {
+            int count;
+            int liveNeighbors;
+            
+            do
+            {
+                count = 0;
+                
+                for(int i = 0; i < board.length; i++)
+                {
+                    for(int j = 0; j < board[0].length; j++)
+                    {
+                        liveNeighbors = calculateNeighborValues(i, j, board);
+                        
+                        if(((board[i][j] & 1) == 1) &&
+                            (liveNeighbors == 2 || liveNeighbors == 3))
+                        {
+                            board[i][j] = 3;//11
+                            count++;
+                        }
+                        else if(board[i][j] == 0 && liveNeighbors == 3)
+                        {
+                            board[i][j] = 2;//10
+                            count++;
+                        }
+                    }
+                }
+                
+                for(int i = 0; i < board.length; i++)
+                {
+                    for(int j = 0; j < board[0].length; j++)
+                    {
+                        board[i][j] = board[i][j] >> 1;
+                    }
+                }
+                
+            }while(count > 0);
+        }
+        
+    }
+    
+    //This is not optimal! Trying to find another way to do this!
+    private int calculateNeighborValues(int i, int j, int[][] board)
+    {
+        int liveNeighbors = 0;
+
+        if(i - 1 >= 0)
+        {
+            if((board[i - 1][j] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(i + 1 <= board.length - 1)
+        {
+            if((board[i + 1][j] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(j - 1 >= 0)
+        {
+            if((board[i][j - 1]  & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(j + 1 <= board[0].length - 1)
+        {
+            if((board[i][j + 1] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(i + 1 <= board.length - 1 && j + 1 <= board[0].length - 1)
+        {
+            if((board[i + 1][j + 1] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(i - 1 >= 0 && j + 1 <= board[0].length - 1)
+        {
+            if((board[i - 1][j + 1] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(i + 1 <= board.length - 1 && j - 1 >= 0)
+        {
+            if((board[i + 1][j - 1] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        if(i - 1 >= 0 && j - 1 >= 0)
+        {
+            if((board[i - 1][j - 1] & 1) == 1)
+            {
+                liveNeighbors++;
+            }
+        }
+        
+        return liveNeighbors;
+    }
+}
